1ca1796288e20d88259700b22bac0b5283647988
Aditya-1406 <gargaditya674@gmail.com>
2025-01-01 16:34:53 +0530

    Missing array

--- gfg/Beginner.java
+++ gfg/Beginner.java
@@ -1,5 +1,7 @@
 package gfg;
 
+import java.util.Arrays;
+
 public class Beginner {
 
     // Array Search
@@ -16,13 +18,39 @@ public class Beginner {
         return res;
     }
 
+    // missing in the array -->
+
+    static int missingNumber(int arr[]) {
+        Arrays.sort(arr);
+        int res = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] != i+1){
+                res = i+1;
+                break;
+            }
+        }
+        if(res == -1) {
+            res = arr.length+1;
+        }
+        return res;
+    }
+
 
     public static void main(String[] args) {
         
         // Array search 
 
-        int arr[] = {1, 2, 3, 4};
-        int x = 3;
-        System.out.println(search(arr, x));
+        int arr[] = {1};
+        // int x = 3;
+        // System.out.println(search(arr, x));
+
+        //----------------------------------------------------
+
+        // Find min and max (solved on gfg)-->
+
+        // -------------------------------------------------------
+
+        // missing element in the array->
+        System.out.println(missingNumber(arr));
     }
 }
