efde3f501b378adc0d70244f3142c04c99c213a8
Aditya-1406 <gargaditya674@gmail.com>
2025-01-01 16:16:01 +0530

    Array search

--- gfg/Beginner.java
+++ gfg/Beginner.java
@@ -1,5 +1,28 @@
 package gfg;
 
 public class Beginner {
-    
+
+    // Array Search
+    static int search(int arr[], int x) {
+
+        int res = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==x){
+                res = i;
+                break;
+            }
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        
+        // Array search 
+
+        int arr[] = {1, 2, 3, 4};
+        int x = 3;
+        System.out.println(search(arr, x));
+    }
 }
