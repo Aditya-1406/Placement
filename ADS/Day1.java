4fabeba0f952f2e365e0a038ed93c52945b0a099
Aditya-1406 <gargaditya674@gmail.com>
2025-01-13 11:54:33 +0530

    Added Optimised code

--- ADS/Day1.java
+++ ADS/Day1.java
@@ -1,18 +1,47 @@
 package ADS;
 
 public class Day1 {
-    public static void main(String[] args) {
+    static void rev(int[] arr,int i, int j){
         
-        int[] arr = {1,2,3,4,5,6,7};
-        int k = 1;
-        int size = k%arr.length;
-        int start = arr.length - size;
-
-        for(int i = start;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
-        for(int i = 0;i<start;i++){
-            System.out.print(arr[i]+" ");
+        while (i<=j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
         }
     }
+    public static void main(String[] args) {
+
+
+        // Rotate the Array--->  brute force
+
+        // int[] arr = {1,2,3,4,5,6,7};
+        // int k = 3;
+
+        // int size = k%arr.length;
+        // int start = arr.length - size;
+
+        // for(int i = start;i<arr.length;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+        // for(int i = 0;i<start;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+
+        //optmised way--->
+
+        // k=k%arr.length;
+        // rev(arr,0,arr.length-1);
+        // rev(arr,0,k-1);
+        // rev(arr,k,arr.length-1);
+        
+        // for (int i : arr) {
+        //     System.out.print(i);
+        // }
+
+        
+        
+    }
+    
 }
